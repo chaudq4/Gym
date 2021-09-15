2ab33ee719c116f96bf50319b597d232fecb8326
chaudq4 <70027297+chaudq4@users.noreply.github.com>
2021-09-15 22:59:22 +0700

    UI update

--- app/src/main/java/com/chauduong/gym/fragment/DetailFragment.java
+++ app/src/main/java/com/chauduong/gym/fragment/DetailFragment.java
@@ -1,10 +1,13 @@
 package com.chauduong.gym.fragment;
 
+import android.app.Dialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,13 @@ import androidx.fragment.app.Fragment;
 
 import com.chauduong.gym.R;
 import com.chauduong.gym.adapter.SliderAdapter;
+import com.chauduong.gym.databinding.DialogVideoBinding;
 import com.chauduong.gym.databinding.FragmentDetailBinding;
 import com.chauduong.gym.model.SliderItem;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -38,7 +44,6 @@ public class DetailFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initData();
         initView();
-        initYoutube();
         initSlider();
     }
 
@@ -61,17 +66,6 @@ public class DetailFragment extends Fragment {
 
     }
 
-    private void initYoutube() {
-        getLifecycle().addObserver(mFragmentDetailBinding.ytVideo);
-        mFragmentDetailBinding.ytVideo.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-            @Override
-            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                String videoId = "Z4HivEWoXGE";
-                youTubePlayer.loadVideo(videoId, 0);
-                youTubePlayer.pause();
-            }
-        });
-    }
 
     private void initView() {
             StringBuilder sb= new StringBuilder();
@@ -79,6 +73,76 @@ public class DetailFragment extends Fragment {
                 sb.append("abcd");
             }
             mFragmentDetailBinding.txtInstruction.setText(sb.toString());
+            mFragmentDetailBinding.txtInstruction.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Dialog dialog = new Dialog(getContext());
+                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    DialogVideoBinding dialogVideoBinding= DataBindingUtil.inflate(getLayoutInflater(),R.layout.dialog_video,null,false);
+                    dialog.setContentView(dialogVideoBinding.getRoot());
+                    dialog.show();
+                    WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    lp.copyFrom(dialog.getWindow().getAttributes());
+                    dialog.getWindow().setAttributes(lp);
+                    getActivity().getLifecycle().addObserver(dialogVideoBinding.ytVideo);
+                    dialogVideoBinding.ytVideo.addYouTubePlayerListener(new YouTubePlayerListener() {
+                        @Override
+                        public void onReady(YouTubePlayer youTubePlayer) {
+                            String videoId = "Z4HivEWoXGE";
+                            youTubePlayer.loadVideo(videoId, 0);
+                            youTubePlayer.pause();
+                        }
+
+                        @Override
+                        public void onStateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlayerState playerState) {
+
+                        }
+
+                        @Override
+                        public void onPlaybackQualityChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackQuality playbackQuality) {
+
+                        }
+
+                        @Override
+                        public void onPlaybackRateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackRate playbackRate) {
+
+                        }
+
+                        @Override
+                        public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError playerError) {
+
+                        }
+
+                        @Override
+                        public void onCurrentSecond(YouTubePlayer youTubePlayer, float v) {
+
+                        }
+
+                        @Override
+                        public void onVideoDuration(YouTubePlayer youTubePlayer, float v) {
+
+                        }
+
+                        @Override
+                        public void onVideoLoadedFraction(YouTubePlayer youTubePlayer, float v) {
+
+                        }
+
+                        @Override
+                        public void onVideoId(YouTubePlayer youTubePlayer, String s) {
+
+                        }
+
+                        @Override
+                        public void onApiChange(YouTubePlayer youTubePlayer) {
+
+                        }
+                    });
+
+                }
+
+            });
 
     }
 
